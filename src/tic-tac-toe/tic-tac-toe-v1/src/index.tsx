20f862037c6b4e0883e04dd747a0f5926cf3e4bc
Jorge Quintana García <alu0101123547@ull.edu.es>
2023-04-22 21:16:59 +0000

    Doc

--- src/tic-tac-toe/tic-tac-toe-v1/src/index.tsx
+++ src/tic-tac-toe/tic-tac-toe-v1/src/index.tsx
@@ -1,3 +1,14 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Jorge Quintana García, Adal Díaz Fariña
+ * @since Apr 22, 2023.
+ * @description File that access the html body 'root' and render the imported component in our html.
+ */
+
 //Used to detect bad uses of react code in a desired piece of code.
 import { StrictMode } from 'react';
 //Used to render a react element into the DOM with render.
@@ -7,8 +18,8 @@ import App from './App.tsx'
 //The styles for your components
 import "./assets/styles.css";
 
-//We access the html body 'root' and render the imported component in our html. 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+//We access the html body 'root' and render the imported component in our html.
+const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
     <App />
--- src/tic-tac-toe/tic-tac-toe-v2/src/App.tsx
+++ src/tic-tac-toe/tic-tac-toe-v2/src/App.tsx
@@ -1,33 +1,47 @@
-/*
-  Now we need nine of them so if we only copy and paste it like this:
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Jorge Quintana García, Adal Díaz Fariña
+ * @since Apr 22, 2023.
+ * @description Now we need nine of them so if we only copy and paste it like this:
+ * @example
+ * ```typescript
+ * 
+ *export default function Square() {
+ *  return <button className="square">X</button><button className="square">X</button>;
+ *}
+ * 
+ * ```
+ * We will get an error because React components can only return a single JSK element.
+ * We have to wrap multiple JSK elements with <> and </> and then return it like this:
+ * @example
+ * ```typescript
+ * 
+ *export default function Square() {
+ *  return (
+ *    <>
+ *      <button className="square">X</button>
+ *      <button className="square">X</button>
+ *    </>
+ *  );
+ *}
+ * 
+ * ``` 
+ * So, now, you should be able to see two squares. We need nine of them so we copy and paste it.
+ * The problem now is that all the squares are in a single row, so we fix it like the code below.
+ */
 
-  export default function Square() {
-    return <button className="square">X</button><button className="square">X</button>;
-  }
-
-  We will get an error because React components can only return a single JSK element.
-  We have to wrap multiple JSK elements with <> and </> and then return it like this:
-
-  export default function Square() {
-    return (
-      <>
-        <button className="square">X</button>
-        <button className="square">X</button>
-      </>
-    );
-  }
-
-  So, now, you should be able to see two squares. We need nine of them so we copy and paste it.
-  The problem now is that all the squares are in a single row, so we fix it like this:
-*/
-
-/*
-  The only 'rare' thing that we see is the className="board-row" that is a 
-  div prop that tells the style to be added to that container.
-
-  And we rename the component because it is not a single square any more.
-*/
-export default function Board() {
+/**
+ * @description The only 'rare' thing that we see is the className="board-row" that is a 
+ * div prop that tells the style to be added to that container.
+ *
+ * And we rename the component because it is not a single square any more.
+ * @returns The component as JSK element.
+ */
+export default function Board(): JSX.Element {
   return (
     <>
       <div className="board-row">
